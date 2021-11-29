b0dbd192e4ae25414f901f51faf842b562af5bf7
Maren Haid <maren.haid@hella.info>
2021-11-29 08:08:43 +0100

    Initial Commit

---
+++ GeometryEffectBug/GeometryEffectBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryEffectBugApp.swift
+//  GeometryEffectBug
+//
+//  Created by Maren Haid on 29.11.21.
+//
+
+import SwiftUI
+
+@main
+struct GeometryEffectBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
