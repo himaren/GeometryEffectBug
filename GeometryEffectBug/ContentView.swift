d5d7626e51314f3b7ac4e1a279e0fdfb10adf924
Maren Haid <maren.haid@hella.info>
2021-11-29 09:36:08 +0100

    initial commit

--- GeometryEffectBug/ContentView.swift
+++ GeometryEffectBug/ContentView.swift
@@ -2,15 +2,51 @@
 //  ContentView.swift
 //  GeometryEffectBug
 //
-//  Created by Maren Haid on 29.11.21.
+//  Created by Maren on 29.11.21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var offset = CGSize.zero
+    @State private var changeAngle: Bool  = false
+    @State private var changeScale: Bool = false
+    
+    let rotationAngle3D = Angle(degrees: 70)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let height: Double = 40
+        let width: Double = 200
+            
+        
+        VStack(spacing: 1) {
+            Rectangle()
+                .fill(.red)
+                .frame(width: width, height: height)
+                .rotation3DEffect(rotationAngle3D, axis: (x: 1, y: 0, z: 0))
+            Rectangle()
+                .fill(.red)
+                .frame(width: width, height: height)
+        }
+        .background(.thinMaterial)
+        .scaleEffect(changeScale ? 0.5 : 1)
+        .rotationEffect(changeAngle ? Angle(degrees: 50.0) : Angle(degrees: 0.0))
+        .offset(self.offset)
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    self.offset = gesture.translation
+                }
+                .onEnded { gesture in
+                    self.offset = CGSize.zero
+                }
+        )
+        .onTapGesture(count: 2) {
+            self.changeScale.toggle()
+            }
+        .onTapGesture(count: 1) {
+            self.changeAngle.toggle()
+            }
     }
 }
 
